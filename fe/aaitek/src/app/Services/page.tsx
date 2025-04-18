9260bd27cec6cd2f162cd72809611b7e55be936a
DESKTOP-6KR30J3\DELL <Divyasonids219@gmail.com>
2025-04-18 10:36:12 +0530

    Added Carousel

--- fe/aaitek/src/app/Services/page.tsx
+++ fe/aaitek/src/app/Services/page.tsx
@@ -1,14 +1,9 @@
 // app/services/page.tsx
+import { HeroCarousel } from '@/components/common/HeroCarousel';
 export default function Services() {
     return (
-      <main className="p-8 text-white bg-black min-h-screen">
-        <h1 className="text-4xl font-bold">Our Services</h1>
-        <ul className="mt-4 list-disc list-inside">
-          <li>Custom test Software Development</li>
-          <li>Web & Mobile App Solutions</li>
-          <li>Cloud & DevOps Services</li>
-          <li>UI/UX Design & Consulting</li>
-        </ul>
-      </main>
+      <div className="bg-white text-gray-800">
+          <HeroCarousel />
+          </div>
     );
   }
