f019a194f1dd9066c8d24d6e3b8b190c3724b0a0
DESKTOP-6KR30J3\DELL <Divyasonids219@gmail.com>
2025-04-18 10:29:18 +0530

    Adding test

--- fe/aaitek/src/app/Services/page.tsx
+++ fe/aaitek/src/app/Services/page.tsx
@@ -4,7 +4,7 @@ export default function Services() {
       <main className="p-8 text-white bg-black min-h-screen">
         <h1 className="text-4xl font-bold">Our Services</h1>
         <ul className="mt-4 list-disc list-inside">
-          <li>Custom Software Development</li>
+          <li>Custom test Software Development</li>
           <li>Web & Mobile App Solutions</li>
           <li>Cloud & DevOps Services</li>
           <li>UI/UX Design & Consulting</li>
